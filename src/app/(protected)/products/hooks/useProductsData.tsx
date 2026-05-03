7ad43249a48be1054f77262ff4fcabd8ce25054f
Gabriel <ghaabriieel@gmail.com>
2026-05-03 03:33:12 -0300

    erro na rota products

--- src/app/(protected)/products/hooks/useProductsData.tsx
+++ src/app/(protected)/products/hooks/useProductsData.tsx
@@ -2,43 +2,47 @@ import { useQuery } from "@tanstack/react-query";
 import { ProductResponseData } from "../interfaces/products";
 import { FilterProductsType } from "../validations/filter-products";
 
+
+
 interface ProductsParams extends FilterProductsType {
     page: number;
     limit: number
-}
+}  
 
 const ProductsData = async (data: ProductsParams): Promise<ProductResponseData> => {
     const params = new URLSearchParams()
-
     if (data.status && data.status !== "all") {
         params.append("status", data.status)
     }
+
 
     if (data.price) params.append("price", data.price)
 
     params.append("page", String(data.page))
     params.append("limit", String(data.limit))
 
+
     const query = params.toString()
     const url = query
-        ? `/api/products?${query}`
-        : `/api/products`
+        ? `${process.env.NEXT_PUBLIC_URL_API}/product?${query}`
+        : `${process.env.NEXT_PUBLIC_URL_API}/product`
 
+    
     const res = await fetch(url, {
         method: 'GET',
+        credentials: "include",
         cache: "no-store",
         headers: {
             "Content-Type": "application/json"
         }
     })
-
     const body: ProductResponseData = await res.json()
     if (!res.ok) {
         throw new Error(body.message || 'Erro ao Filtrar Produtos')
     }
-
     return body
 }
+
 
 export function useProductsData(
     filters: FilterProductsType,
@@ -55,4 +59,4 @@ export function useProductsData(
             }),
         staleTime: 1000 * 60 * 5,
     });
-} 
+}
